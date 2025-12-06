9435047d3b8f51d4d04bd4813ef94dca2c9a8528
Karl robeck alferez <karlalferezfx@gmail.com>
2025-12-06 22:56:32 +0800

    feat: remove AccordionExample1 story from Accordion stories

--- stories/Accordion.stories.tsx
+++ stories/Accordion.stories.tsx
@@ -19,34 +19,3 @@ const meta = preview.meta({
     backgroundColor: { control: "color" },
   },
 });
-
-export const AccordionExample1 = meta.story({
-  render: () => {
-    return (
-      <div class="min-w-xl">
-        <Accordion multiple={false} collapsible class="w-full">
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-2">
-            <AccordionTrigger>Is it styled?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It comes with default styles that matches the other
-              components' aesthetic.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-3">
-            <AccordionTrigger>Is it animated?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It's animated by default, but you can disable it if you
-              prefer.
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-      </div>
-    );
-  },
-});
